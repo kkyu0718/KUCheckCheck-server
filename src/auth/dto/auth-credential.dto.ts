e4c5ead893c0ffe7f457a59204bdc6cac4b953e7
limheesun4898 <heehee4898@gmail.com>
2022-09-08 19:04:17 +0900

    commit test

--- src/auth/dto/auth-credential.dto.ts
+++ src/auth/dto/auth-credential.dto.ts
@@ -1,28 +1,23 @@
-
 export class SignUpInfoDto {
-    // 필수 입력
-    email: string;
-    password: string;
-    name: string;
-    comment: string;
-    
-    // 선택사항
-    detail_comment?: string;
-    github_link?: string;
-    instagram_link?: string;
+  // 필수 입력
+  email: string;
+  password: string;
+  name: string;
+  comment: string;
+
+  // 선택사항
+  detail_comment?: string;
+  github_link?: string;
+  instagram_link?: string;
 }
 
 export class SignInInfoDto {
-    
-    // 필수 입력
-    email: string;
-    password: string;
-    
+  // 필수 입력
+  email: string;
+  password: string;
 }
 
-
 export class MyInfoDto {
-    
-    accessToken: string;
-    // accessToken : name, role, email
+  accessToken: string;
+  // accessToken : name, role, email
 }
