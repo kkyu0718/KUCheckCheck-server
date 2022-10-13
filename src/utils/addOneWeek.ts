388fe06817e779c54c6ee2389675a62618bb255b
limheesun4898 <heehee4898@gmail.com>
2022-10-13 18:14:30 +0900

    feat: 특정 주차 타입 수정

--- src/utils/addOneWeek.ts
+++ src/utils/addOneWeek.ts
@@ -3,7 +3,7 @@ import * as customParseFormat from 'dayjs/plugin/customParseFormat';
 import * as utc from 'dayjs/plugin/utc';
 import * as tz from 'dayjs/plugin/timezone';
 
-function addOneWeek(week: string): Date {
+function addOneWeek(week: Date): Date {
   dayjs.extend(customParseFormat);
   dayjs.extend(utc);
   return dayjs.utc(week).tz('Asia/Seoul').add(1, 'week').toDate();
--- src/utils/isBetweenWeek.ts
+++ src/utils/isBetweenWeek.ts
@@ -2,7 +2,7 @@ import { addOneWeek } from 'src/utils/addOneWeek';
 import * as dayjs from 'dayjs';
 import * as isBetween from 'dayjs/plugin/isBetween';
 
-function isBetweenWeek(date: Date, week: string): boolean {
+function isBetweenWeek(date: Date, week: Date): boolean {
   dayjs.extend(isBetween);
   const oneWeekAfter = addOneWeek(week);
   return dayjs(date).isBetween(week, oneWeekAfter, 'day', '(]');
