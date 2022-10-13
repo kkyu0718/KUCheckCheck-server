6258613780ada23c2060be932cff8e7f82c11775
limheesun4898 <heehee4898@gmail.com>
2022-10-13 14:46:21 +0900

    feat: timeZone를 mysql의 timezone 사용하는 걸로 수정

--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -12,6 +12,6 @@ export const typeORMConfig: TypeOrmModuleOptions = {
   database: process.env.RDS_DB_NAME || dbConfig.database,
   entities: [__dirname + '/../**/*.entity.{js,ts}'],
   synchronize: dbConfig.synchronize,
-  timezone: 'Asia/Seoul',
+  timezone: 'local',
   dateStrings: true,
 };
