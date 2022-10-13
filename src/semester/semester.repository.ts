4ce0c7fc65724e8f7bae6c7104c8596025f92b65
limheesun4898 <heehee4898@gmail.com>
2022-10-13 14:46:58 +0900

    feat: semester timezone 수정

--- src/semester/semester.repository.ts
+++ src/semester/semester.repository.ts
@@ -2,7 +2,6 @@ import { ConflictException, Injectable } from '@nestjs/common';
 import { DataSource, Repository } from 'typeorm';
 import { CreateSemesterDto } from './dto/semester.dto';
 import { semester } from './semester.entity';
-import { convertTimeZone } from 'src/utils/convertTimeZone';
 
 @Injectable()
 export class SemesterRepository extends Repository<semester> {
@@ -16,21 +15,13 @@ export class SemesterRepository extends Repository<semester> {
 
   async createSemester(createSemesterDto: CreateSemesterDto) {
     try {
-
       const savedData = await this.save(createSemesterDto);
       const { id, createdBy } = savedData;
-      const columns = [
-        'registerStart',
-        'registerEnd',
-        'enrollmentStart',
-        'enrollmentEnd',
-        'activeStart',
-        'activeEnd',
-      ];
       await this.update(id, { updatedBy: createdBy });
-      return convertTimeZone(columns, await this.findOneBy({ id }));
+
+      return await this.findOneBy({ id });
     } catch (error) {
-      console.log(error)
+      console.log(error);
       if (error.code === '23505') {
         throw new ConflictException('해당 학기가 이미 존재합니다');
       }
