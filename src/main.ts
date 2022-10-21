bd6521fadf236d829107cdbc3744ab07525d2b35
kkyu0718 <net605@naver.com>
2022-10-21 23:30:32 +0900

    feat: 전역 예외 처리 필터 적용

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as config from 'config';
+import { GlobalExceptionFilter } from './auth/filter/global-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const serverConfig = config.get('server');
   const port = serverConfig.port;
 
+  app.useGlobalFilters(new GlobalExceptionFilter());
   app.enableCors();
   await app.listen(port);
 }
