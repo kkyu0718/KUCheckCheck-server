d55a063f82bf62b82f9b41b9408016d126b169d6
limheesun4898 <heehee4898@gmail.com>
2022-10-24 00:56:28 +0900

    feat: 공통 response interceptor 추가

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as config from 'config';
+import { TransformationInterceptor } from './global/TransformationInterceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -8,6 +9,7 @@ async function bootstrap() {
   const port = serverConfig.port;
 
   app.enableCors();
+  app.useGlobalInterceptors(new TransformationInterceptor());
   await app.listen(port);
 }
 bootstrap();
