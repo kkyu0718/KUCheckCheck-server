cacc339a7066375c52d91cc7eb9afb250ba9d10f
kkyu0718 <net605@naver.com>
2022-10-25 00:46:50 +0900

    feat: main.ts global-exception-filter 추가

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as config from 'config';
 import { TransformationInterceptor } from './global/TransformationInterceptor';
-import { GlobalExceptionFilter } from './auth/filter/global-exception.filter';
+import { GlobalExceptionFilter } from './global/global-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
