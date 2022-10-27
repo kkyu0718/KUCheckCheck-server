02d1836b18262539b9a6a0382a0ac0225fcb02aa
limheesun4898 <heehee4898@gmail.com>
2022-10-27 12:06:53 +0900

    feat: validationPipe transform 추가

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,17 @@ import { AppModule } from './app.module';
 import * as config from 'config';
 import { TransformationInterceptor } from './global/TransformationInterceptor';
 import { GlobalExceptionFilter } from './global/global-exception.filter';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const serverConfig = config.get('server');
   const port = serverConfig.port;
-
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+    }),
+  );
   app.useGlobalFilters(new GlobalExceptionFilter());
   app.useGlobalInterceptors(new TransformationInterceptor());
   app.enableCors();
